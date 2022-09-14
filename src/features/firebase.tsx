4d5142a7681e984f379e31d7e019a9fc556b9936
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-09-14 10:22:27 +0200

    Add env variables for firebaseConfig

--- src/features/firebase.tsx
+++ src/features/firebase.tsx
@@ -11,12 +11,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: '***REMOVED***',
-    authDomain: '***REMOVED***',
-    projectId: '***REMOVED***',
-    storageBucket: '***REMOVED***.appspot.com',
-    messagingSenderId: '***REMOVED***',
-    appId: '1:***REMOVED***:web:2e8263965710668c6773e4',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
